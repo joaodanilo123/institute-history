b5e00ab68d198104ac95dd434e841be2672d6b5b
joao_danilo <jddiedrich@gmail.com>
2019-11-02 11:34:28 -0300

    att

--- includes/login-validator.php
+++ includes/login-validator.php
@@ -8,10 +8,11 @@ if (isset($_POST['email']) and isset($_POST['senha'])) {
     $email = $_POST['email'];
     $senha = md5($_POST['senha']);
 
-    $sql = "SELECT usuario_nome, usuario_foto FROM usuario WHERE usuario_email = '$email' AND usuario_senha = '$senha'";
+    $sql = "SELECT usuario_nome, usuario_foto, usuario_id FROM usuario WHERE usuario_email = '$email' AND usuario_senha = '$senha'";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result) > 0){
         $row = mysqli_fetch_assoc($result);
+        $_SESSION['id'] = $row['usuario_id'];
         $_SESSION['nome'] = $row['usuario_nome'];
         $_SESSION['foto'] = $row['usuario_foto'];
         header('Location: ../public/index.php');
--- public/nova-foto.php
+++ public/nova-foto.php
@@ -8,35 +8,6 @@ $usuario_logado = isset($_SESSION['nome']) ? 1 : 0;
 <head>
     <?php include "./Components/html-head.php"; ?>
     <title>Nova Foto - Institute History</title>
-    <style>
-        .fotos {
-            padding: 10px;
-        }
-        .add-foto {
-            float: left;
-            height: 200px;
-            width: 200px;
-            border: 1px solid black;
-        }
-
-        .add-foto .btn {
-            width: 50px;
-            height: 50px;
-            position: absolute;
-            left: 75px;
-            top: 80px;
-            z-index: 1000;
-            padding: 5px;
-            background: rgba(76, 175, 80, 1);
-            border: none;
-            box-shadow: none;
-        }
-
-        .add-foto .btn:hover {
-            background: rgba(76, 175, 80, 1);
-            color: white;
-        }
-    </style>
 </head>
 
 <body>
@@ -45,20 +16,27 @@ $usuario_logado = isset($_SESSION['nome']) ? 1 : 0;
         <div class="row">
             <h2 class="header">Nova foto</h2>
             <div class="col s12">
-                <form action="" method="POST" enctype="multipart/form-data">
-                    <div class="input-field">
+                <form action="../includes/cadastro-foto.php" method="POST" enctype="multipart/form-data" class="row">
+                    <div class="input-field col s12">
                         <input type="text" name="titulo" id="titulo">
                         <label for="titulo">Titulo da postagem</label>
                     </div>
-                    <div id="fotos">
-                        <div class="file-field input-field add-foto">
-                            <img class="preview" width="200" height="200">
-                            <div class="btn" id="btn0">
-                                <span><i class="material-icons">add</i></span>
-                                <input type="file" name="foto0" required />
-                            </div>
+                    <div class="input-field col s12">
+                        <textarea id="textarea" class="materialize-textarea" name="resumo"></textarea>
+                        <label for="textarea">Resumo</label>
+                    </div>
+
+
+                    <div class="file-field input-field col s12">
+                        <div class="btn">
+                            <span>File</span>
+                            <input type="file" name="fotos[]" multiple>
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Upload one or more files">
                         </div>
                     </div>
+
                     <div class="row">
                         <input class="btn col s12 m12 green" type="submit" value="cadastrar">
                     </div>
@@ -67,60 +45,13 @@ $usuario_logado = isset($_SESSION['nome']) ? 1 : 0;
             <div class="col s12 m6">
                 <?php if (isset($_SESSION['erro'])) : ?>
                     <span class="badge red white-text"><?= $_SESSION['erro'] ?></span>
-                <?php endif;
-                session_destroy();
+                <?php
+                    session_destroy();
+                endif;
                 ?>
             </div>
         </div>
-
     </div>
-    <script>
-        //pega todos os inputs e fotos preview
-        var previews = document.querySelectorAll('.preview');
-        var inputs = document.querySelectorAll('input[type=file]');
-
-        //define todos os inputs como não alterados
-        inputs.forEach(element => {
-            element.mudado = false;
-        })
-
-        //função pra adionar um input de foto
-        function newFileInput(index_foto) {
-            document.getElementById('fotos').innerHTML +=
-                '<div class="file-field input-field add-foto"><img class="preview" width="200" height="200"><div class="btn" id="btn'+ index_foto +'"><span><i class="material-icons">add</i></span><input type="file" name="btn' + index_foto + '" /></div></div> '
-        }
-
-        //função pra remover o botão de + em um preview
-        function removerBtn(btnID){
-            document.querySelector(`#${btnID}`).style.display = 'none';
-        }
-
-        //define um trigger pra todos os inputs(recursivamente)
-        function addEvent() {
-            inputs.forEach((value, index) => {
-                value.addEventListener('change', (e) => {
-                    //bagulhos copiados do StackOverflow
-                    const fileToUpload = e.target.files.item(0);
-                    const reader = new FileReader();
-                    reader.onload = e => previews[index].src = e.target.result;
-                    reader.readAsDataURL(fileToUpload);
-                    
-                    //cria novo input de foto
-                    newFileInput(index + 1)
-                    
-                    //removove o botão de + do elemento atual na iteração
-                    removerBtn('btn' + index)
-
-                    //atualiza as variaveis iniciais
-                    previews = document.querySelectorAll('.preview');
-                    inputs = document.querySelectorAll('input[type=file]');
-                    addEvent();
-                });
-            });
-        }
-
-        addEvent()
-    </script>
 </body>
 
 </html>
